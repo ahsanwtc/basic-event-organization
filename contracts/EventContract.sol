c2265838b9e21e845d7a2770b6f5063c52e1b050
jsan <ahsan.wtc@gmail.com>
2022-03-31 16:18:46 +0500

    starting testing

--- contracts/EventContract.sol
+++ contracts/EventContract.sol
@@ -1,7 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.1;
+// import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract EventContract {
-  constructor() public {
+  struct Event {
+    uint id;
+    address admin;
+    string name;
+    uint date;
+    uint price;
+    uint ticketCount;
+    uint ticketRemaining;
+  }
+  mapping(uint => Event) public events;
+  mapping(address => mapping(uint => uint)) public tickets;
+  uint public nextEventId;
+
+  function createEvent(string calldata name, uint date, uint price, uint ticketCount) external {
+    require(date > block.timestamp, "an event can only be created at a future date");
+    require(ticketCount > 0, "an event can only be created with 1 or more tickets");
+    events[nextEventId] = Event(nextEventId, msg.sender, name, date, price, ticketCount, ticketCount);
+    nextEventId++;
+  }
+
+  function buyTicket(uint eventId, uint quantity) payable external eventExist(eventId) eventActive(eventId) {
+    Event storage _event = events[eventId];       
+    require(_event.ticketRemaining >= quantity, "not enough tickets left");
+    require(msg.value == (quantity * _event.price), "not enough ether sent");
+    _event.ticketRemaining -= quantity;
+    tickets[msg.sender][eventId] += quantity;
+  }
+
+  function transferTicket(uint eventId, uint quantity, address to) external eventExist(eventId) eventActive(eventId) {
+    require(tickets[msg.sender][eventId] >= quantity, "not enough tickets");
+    tickets[msg.sender][eventId] -= quantity;
+    tickets[to][eventId] += quantity;
+  }
+
+  modifier eventExist(uint eventId) {
+    require(events[eventId].date > 0, "event does not exist");
+    _;
+  }
+
+  modifier eventActive(uint eventId) {
+    require(block.timestamp < events[eventId].date, "event is not active anymore");
+    _;
   }
 }
--- test/EventContract.test.js
+++ test/EventContract.test.js
@@ -1,3 +1,4 @@
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
 const EventContract = artifacts.require("EventContract");
 
 /*
@@ -5,9 +6,23 @@ const EventContract = artifacts.require("EventContract");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("EventContract", function (/* accounts */) {
-  it("should assert true", async function () {
-    await EventContract.deployed();
-    return assert.isTrue(true);
+contract("EventContract", accounts => {
+  let eventContract = null;
+
+  before(async () => {
+    eventContract = await EventContract.new();
   });
+
+  it('creates an event', async () => {
+    const price = 10, date = (await time.latest()).add(time.duration.seconds(4000)).toNumber(), ticketCount = 10;
+    await eventContract.createEvent('Event One', date, price, ticketCount, { from: accounts[0] });
+    const event = await eventContract.events(0);
+    assert(event.name === 'Event One');
+    assert(event.price.toNumber() === price);
+    assert(event.date.toNumber() === date);
+    assert(event.ticketCount.toNumber() === ticketCount);
+    assert(event.ticketRemaining.toNumber() === ticketCount);
+    assert(event.admin === accounts[0]);
+  });
+
 });
