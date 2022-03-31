ab76061dd40383956ed1fe9dd305c5e9cca79740
jsan <ahsan.wtc@gmail.com>
2022-03-31 16:49:08 +0500

    wrote tests for buy tickets

--- test/EventContract.test.js
+++ test/EventContract.test.js
@@ -41,4 +41,59 @@ contract("EventContract", accounts => {
     );
   });
 
+  context('buyTickt', () => {
+    let event = null;
+    before(async () => {
+      const price = 10, date = (await time.latest()).add(time.duration.seconds(4000)).toNumber(), ticketCount = 10;
+      await eventContract.createEvent('Event', date, price, ticketCount, { from: accounts[0] });
+      event = await eventContract.events(0);
+    });
+
+    it('should buy tickets', async () => {
+      const quantity = 4;
+      await eventContract.buyTicket(event.id, quantity, { from: accounts[1], value: quantity * event.price });
+      assert((await eventContract.tickets(accounts[1], event.id)).toNumber() === quantity);
+      assert((await eventContract.events(event.id)).ticketRemaining.toNumber() === (event.ticketCount -  quantity));
+    });
+
+    it('should NOT buy tickets when no enough tickts left', async () => {
+      const quantity = 11;
+      expectRevert(
+        eventContract.buyTicket(event.id, quantity, { from: accounts[1], value: quantity * event.price }),
+        'not enough tickets left'
+      );
+    });
+
+    it('should NOT buy tickets when not enough ether sent', async () => {
+      const quantity = 5;
+      expectRevert(
+        eventContract.buyTicket(event.id, quantity, { from: accounts[1], value: (quantity * event.price) - 5 }),
+        'not enough ether sent'
+      );
+    });
+
+    it('should NOT buy tickets when event does not exist', async () => {
+      const quantity = 5;
+      expectRevert(
+        eventContract.buyTicket(1, quantity, { from: accounts[1], value: (quantity * event.price) - 5 }),
+        'event does not exist'
+      );
+    });
+
+    it('should NOT buy tickets when event is not active', async () => {
+      const eventContract = await EventContract.new();
+      const price = 10, date = (await time.latest()).add(time.duration.seconds(5)).toNumber(), ticketCount = 10;
+      await eventContract.createEvent('Event', date, price, ticketCount, { from: accounts[0] });
+      const event = await eventContract.events(0);
+      await time.increase(5001);
+
+      const quantity = 5;
+      expectRevert(
+        eventContract.buyTicket(0, quantity, { from: accounts[1], value: (quantity * event.price) }),
+        'event is not active anymore'
+      );
+    });
+
+  });
+
 });
