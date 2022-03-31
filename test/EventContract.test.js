1f8f28b9a647b4a7221fb2ce079c8ecd874c88a0
jsan <ahsan.wtc@gmail.com>
2022-03-31 16:22:32 +0500

    wrote tests for create event

--- test/EventContract.test.js
+++ test/EventContract.test.js
@@ -13,16 +13,32 @@ contract("EventContract", accounts => {
     eventContract = await EventContract.new();
   });
 
-  it('creates an event', async () => {
+  it('should create an event', async () => {
     const price = 10, date = (await time.latest()).add(time.duration.seconds(4000)).toNumber(), ticketCount = 10;
-    await eventContract.createEvent('Event One', date, price, ticketCount, { from: accounts[0] });
+    await eventContract.createEvent('Event', date, price, ticketCount, { from: accounts[0] });
     const event = await eventContract.events(0);
-    assert(event.name === 'Event One');
+    assert(event.name === 'Event');
     assert(event.price.toNumber() === price);
     assert(event.date.toNumber() === date);
     assert(event.ticketCount.toNumber() === ticketCount);
     assert(event.ticketRemaining.toNumber() === ticketCount);
     assert(event.admin === accounts[0]);
+  });
+
+  it('should NOT create a past event', async () => {
+    const price = 10, date = (await time.latest()).sub(time.duration.seconds(5)).toNumber(), ticketCount = 10;
+    await expectRevert(
+      eventContract.createEvent('Event', date, price, ticketCount, { from: accounts[0] }),
+      'an event can only be created at a future date'
+    );
+  });
+
+  it('should NOT create an event with no tickets', async () => {
+    const price = 10, date = (await time.latest()).add(time.duration.seconds(1000)).toNumber(), ticketCount = 0;
+    await expectRevert(
+      eventContract.createEvent('Event', date, price, ticketCount, { from: accounts[0] }),
+      'an event can only be created with 1 or more tickets'
+    );
   });
 
 });
